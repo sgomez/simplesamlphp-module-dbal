0a04b6a12c67012deb29e702efbd3552e9b069c2
Sergio Gómez <sergio@uco.es>
2015-05-04 22:07:12 +0200

    Added config template file

---
+++ config-templates/module_dbal.php
@@ -0,0 +1,15 @@
+<?php
+
+$config = array (
+    /*
+     * The Doctrine/DBAL datastore should connect to.
+     *
+     * See http://doctrine-dbal.readthedocs.org/en/latest/reference/configuration.html
+     * for the various syntaxes.
+     */
+    'store.dbal.dbname'             => 'simplesamlphp',
+    'store.dbal.user'               => 'user',
+    'store.dbal.password'           => 'password',
+    'store.dbal.host'               => 'localhost',
+    'store.dbal.driver'             => 'pdo_mysql',
+);
