811c919ea765d5d787994c3655c4962ab8020974
Sergio Gomez <sergio@uco.es>
2017-06-13 18:34:41 +0200

    Support connection using a database URL

--- config-templates/module_dbal.php
+++ config-templates/module_dbal.php
@@ -7,9 +7,16 @@ $config = array (
      * See http://doctrine-dbal.readthedocs.org/en/latest/reference/configuration.html
      * for the various syntaxes.
      */
-    'store.dbal.dbname'             => 'simplesamlphp',
-    'store.dbal.user'               => 'user',
-    'store.dbal.password'           => 'password',
-    'store.dbal.host'               => 'localhost',
-    'store.dbal.driver'             => 'pdo_mysql',
+
+    'store.dbal.url'                => 'mysql://user:password@localhost:3306/simplesamlphp?charset=utf8mb4&serverVersion=5.7',
+    // 'store.dbal.url'                => 'sqlite:///simplesamlphp.sqlite',
+
+
+    // This way is deprecated
+
+    //    'store.dbal.dbname'             => 'simplesamlphp',
+    //    'store.dbal.user'               => 'user',
+    //    'store.dbal.password'           => 'password',
+    //    'store.dbal.host'               => 'localhost',
+    //    'store.dbal.driver'             => 'pdo_mysql',
 );
